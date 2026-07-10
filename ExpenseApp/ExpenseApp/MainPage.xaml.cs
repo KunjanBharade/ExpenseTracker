ba80df101dfcace71c472b096aa398c2fab62f45
KunjanPersonal <kunjanpbharade@gmail.com>
2026-07-10 14:09:03 +0530

    Display transactions using CollectionView

--- ExpenseApp/ExpenseApp/MainPage.xaml.cs
+++ ExpenseApp/ExpenseApp/MainPage.xaml.cs
@@ -7,58 +7,30 @@
         public MainPage()
         {
             InitializeComponent();
-
-            //var path = @"C:\Users\KBharade\LearnAndBuild\ExpenseTracker_LAB\ExpenseApp\ExpenseApp\Samples\sample_statement.csv";
-
-            //var lines = File.ReadAllLines(path);
-
-            //foreach (var line in lines.Skip(1))
-            //{
-            //    var parts = line.Split(',');
-
-            //    System.Diagnostics.Debug.WriteLine(
-            //        $"Date: {parts[0]}, Description: {parts[1]}, Amount: {parts[2]}");
-            //}
-
-            var transactions = new List<Transaction>
-            {
-                new Transaction
-                {
-                    Description = "Salary",
-                    Amount = 39000,
-                    Category = "Income"
-                },
-                new Transaction
-                {
-                    Description = "Rent",
-                    Amount = -6125,
-                    Category = "Housing"
-                },
-                new Transaction
-                {
-                    Description = "Swiggy",
-                    Amount = -250,
-                    Category = "Food"
-                }
-            };
-
-            foreach (var transaction in transactions)
-            {
-                System.Diagnostics.Debug.WriteLine(
-                    $"{transaction.Description} : {transaction.Amount}");
-            }
+            TransactionsList.ItemsSource = transactions;
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+        private List<Transaction> transactions = new()
         {
-            count++;
+            new Transaction
+            {
+                Description = "Salary",
+                Amount = 39000,
+                Category = "Income"
+            },
+            new Transaction
+            {
+                Description = "Rent",
+                Amount = -6125,
+                Category = "Housing"
+            },
+            new Transaction
+            {
+                Description = "Swiggy",
+                Amount = -250,
+                Category = "Food"
+            }
+        };
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
-    }
+    }    
 }
